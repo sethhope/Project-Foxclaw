30c5e8fda8a71577a8023bf51af96d90dfe01310
sethhope <support@jcamtech.com>
2014-11-17 12:04:01 -0500

    Updated Collider Class
    - Added function definitions for most collider functions
    - Fixed damping to handle linear and angular damping

--- Objects/Collider.cpp
+++ Objects/Collider.cpp
@@ -1,8 +1,69 @@
 #include "Collider.h"
 using namespace FCE;
 
-COLLIDER::COLLIDER(scene::ISceneNode* parent, LOGGER* log)
+COLLIDER::COLLIDER(NODE* parent, LOGGER* log)
 {
 	this->log = log;
+	if(type >= MAX_TYPE)
+	{
+		this->type = type;
+	}
+	else
+	{
+		log->logData("Invalid type", type);
+	}
+	parent->attach((NODE)this);
+}
+
+COLLIDER::~COLLIDER()
+{
 	
+}
+
+void COLLIDER::onInit()
+{
+	//TODO: add init things
+	//create rigid body
+	//attach body to the node
+}
+
+void COLLIDER::onUpdate()
+{
+}
+
+void COLLIDER::onRender()
+{
+	
+}
+
+void COLLIDER::setType(int type)
+{
+	if(type >= MAX_TYPE)
+	{
+		this->type = type;
+	}
+	else
+	{
+		log->logData("Invalid type", type);
+	}
+}
+
+void COLLIDER::setDamping(float lDamping, float aDamping)
+{
+	body->setDamping(lDamping, aDamping);
+}
+
+void COLLIDER::setFriction(float friction)
+{
+	body->setFriction(friction);
+}
+
+void COLLIDER::setMass(float mass)
+{
+	body->setMassProps(mass, core::vector3df(0, 0, 0));
+}
+
+void COLLIDER::setVelocity(core::vector3df velocity)
+{
+	body->setAngularVelocity(velocity);
 }
